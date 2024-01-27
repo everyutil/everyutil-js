f33ff2a1c373a0e08c438de945fcd1ee70900b4c
dailker <ilkeroztbm@gmail.com>
2024-01-27 11:56:31 -0500

    Add src/logic/conjoin.ts

---
+++ src/logic/conjoin.ts
@@ -0,0 +1,9 @@
+/**
+ * Combines multiple predicates into one using logical AND.
+ * @author @dailker
+ * @param predicates Array of predicate functions.
+ * @returns A predicate that returns true if all predicates return true.
+ */
+export function conjoin<T extends any[]>(...predicates: Array<(...args: T) => boolean>): (...args: T) => boolean {
+  return (...args: T) => predicates.every(fn => fn(...args));
+}
