36bf9f63a679d086d9ad01d66fe8aca5eda85740
dailker <ilkeroztbm@gmail.com>
2024-01-31 11:50:02 -0500

    Add src/logic/negate.ts

---
+++ src/logic/negate.ts
@@ -0,0 +1,9 @@
+/**
+ * Returns the logical negation of a predicate function.
+ * @author @dailker
+ * @param predicate The predicate function to negate.
+ * @returns A new predicate that returns the negation of the original.
+ */
+export function negate<T extends any[]>(predicate: (...args: T) => boolean): (...args: T) => boolean {
+  return (...args: T) => !predicate(...args);
+}
