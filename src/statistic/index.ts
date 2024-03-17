73576f252e4a24946f67a4fee615666bda463cf7
dailker <ilkeroztbm@gmail.com>
2024-03-17 12:29:19 -0400

    Add src/statistic/index.ts

---
+++ src/statistic/index.ts
@@ -0,0 +1,9 @@
+export * from './mean';
+export * from './median';
+export { mode as statisticMode } from './mode';
+export * from './variance';
+export * from './standardDeviation';
+export * from './range';
+export * from './quartiles';
+export { interquartileRange as statisticInterquartileRange } from './interquartileRange';
+// ...more exports will be added as utilities are implemented
