91146b3106e1865d7756ece30b8987a94635910e
dailker <ilkeroztbm@gmail.com>
2024-07-11 21:18:43 -0400

    Add test/date/addBusinessDays.test.ts

---
+++ test/date/addBusinessDays.test.ts
@@ -0,0 +1,7 @@
+import { addBusinessDays } from '../../src/date/addBusinessDays';
+
+describe('addBusinessDays', () => {
+  it('should be a function', () => {
+    expect(typeof addBusinessDays).toBe('function');
+  });
+});
