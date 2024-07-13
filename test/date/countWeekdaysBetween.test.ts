e949789552b6d77aaa3bc5ecb20266c0444035dd
dailker <ilkeroztbm@gmail.com>
2024-07-13 19:28:18 -0400

    Add test/date/countWeekdaysBetween.test.ts

---
+++ test/date/countWeekdaysBetween.test.ts
@@ -0,0 +1,7 @@
+import { countWeekdaysBetween } from '../../src/date/countWeekdaysBetween';
+
+describe('countWeekdaysBetween', () => {
+  it('should be a function', () => {
+    expect(typeof countWeekdaysBetween).toBe('function');
+  });
+});
