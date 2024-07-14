e38f45bcc4248477754300526ae043805cf6ce13
dailker <ilkeroztbm@gmail.com>
2024-07-14 16:56:24 -0400

    Add test/date/dateAddWorkingHours.test.ts

---
+++ test/date/dateAddWorkingHours.test.ts
@@ -0,0 +1,7 @@
+import { dateAddWorkingHours } from '../../src/date/dateAddWorkingHours';
+
+describe('dateAddWorkingHours', () => {
+  it('should be a function', () => {
+    expect(typeof dateAddWorkingHours).toBe('function');
+  });
+});
