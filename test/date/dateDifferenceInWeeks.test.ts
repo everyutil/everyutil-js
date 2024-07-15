3cb53a3d0d16970450c3077670af92235fbfae5c
dailker <ilkeroztbm@gmail.com>
2024-07-15 19:11:39 -0400

    Add test/date/dateDifferenceInWeeks.test.ts

---
+++ test/date/dateDifferenceInWeeks.test.ts
@@ -0,0 +1,7 @@
+import { dateDifferenceInWeeks } from '../../src/date/dateDifferenceInWeeks';
+
+describe('dateDifferenceInWeeks', () => {
+  it('should be a function', () => {
+    expect(typeof dateDifferenceInWeeks).toBe('function');
+  });
+});
