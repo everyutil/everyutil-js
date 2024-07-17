d6a776743a8bd51a603d649b31f0f02672cd0bb7
dailker <ilkeroztbm@gmail.com>
2024-07-17 19:54:22 -0400

    Add test/date/dateRange.test.ts

---
+++ test/date/dateRange.test.ts
@@ -0,0 +1,7 @@
+import { dateRange } from '../../src/date/dateRange';
+
+describe('dateRange', () => {
+  it('should be a function', () => {
+    expect(typeof dateRange).toBe('function');
+  });
+});
