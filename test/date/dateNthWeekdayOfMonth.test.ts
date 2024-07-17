a579a87fdba6f48fbf60602f9ffc4ff6b8871b5d
dailker <ilkeroztbm@gmail.com>
2024-07-17 07:59:18 -0400

    Add test/date/dateNthWeekdayOfMonth.test.ts

---
+++ test/date/dateNthWeekdayOfMonth.test.ts
@@ -0,0 +1,7 @@
+import { dateNthWeekdayOfMonth } from '../../src/date/dateNthWeekdayOfMonth';
+
+describe('dateNthWeekdayOfMonth', () => {
+  it('should be a function', () => {
+    expect(typeof dateNthWeekdayOfMonth).toBe('function');
+  });
+});
