c89ed816bc4362b79f0a7a021fc8a8987b8f9015
dailker <ilkeroztbm@gmail.com>
2024-07-18 10:37:16 -0400

    Add test/date/daysInMonth.test.ts

---
+++ test/date/daysInMonth.test.ts
@@ -0,0 +1,7 @@
+import { daysInMonth } from '../../src/date/daysInMonth';
+
+describe('daysInMonth', () => {
+  it('should be a function', () => {
+    expect(typeof daysInMonth).toBe('function');
+  });
+});
