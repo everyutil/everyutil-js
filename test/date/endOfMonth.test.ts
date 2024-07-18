3ee32cf92a8cbd2064f3b7cb5a50d86d066f0d16
dailker <ilkeroztbm@gmail.com>
2024-07-18 18:55:12 -0400

    Add test/date/endOfMonth.test.ts

---
+++ test/date/endOfMonth.test.ts
@@ -0,0 +1,7 @@
+import { endOfMonth } from '../../src/date/endOfMonth';
+
+describe('endOfMonth', () => {
+  it('should be a function', () => {
+    expect(typeof endOfMonth).toBe('function');
+  });
+});
