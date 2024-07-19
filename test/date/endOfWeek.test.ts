4f5e5a03f78628c804c17057e9488e1fec2c8d65
dailker <ilkeroztbm@gmail.com>
2024-07-19 19:43:35 -0400

    Add test/date/endOfWeek.test.ts

---
+++ test/date/endOfWeek.test.ts
@@ -0,0 +1,7 @@
+import { endOfWeek } from '../../src/date/endOfWeek';
+
+describe('endOfWeek', () => {
+  it('should be a function', () => {
+    expect(typeof endOfWeek).toBe('function');
+  });
+});
