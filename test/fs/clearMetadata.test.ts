68f19a8c35b2ad363f2cc239c2036a49b189d751
dailker <ilkeroztbm@gmail.com>
2024-07-24 11:42:45 -0400

    Add test/fs/clearMetadata.test.ts

---
+++ test/fs/clearMetadata.test.ts
@@ -0,0 +1,7 @@
+import { clearMetadata } from '../../src/fs/clearMetadata';
+
+describe('clearMetadata', () => {
+  it('should be a function', () => {
+    expect(typeof clearMetadata).toBe('function');
+  });
+});
