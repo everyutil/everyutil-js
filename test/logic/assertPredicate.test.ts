f025d36d0839f6ef2dc17b6cffaea1a24fa0fa26
dailker <ilkeroztbm@gmail.com>
2024-07-31 16:23:07 -0400

    Add test/logic/assertPredicate.test.ts

---
+++ test/logic/assertPredicate.test.ts
@@ -0,0 +1,7 @@
+import { assertPredicate } from '../../src/logic/assertPredicate';
+
+describe('assertPredicate', () => {
+  it('should be a function', () => {
+    expect(typeof assertPredicate).toBe('function');
+  });
+});
