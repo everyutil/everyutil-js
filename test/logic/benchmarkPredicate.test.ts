acfb5340fa6879e2662ee0335a6b8f96b1ea195c
dailker <ilkeroztbm@gmail.com>
2024-08-01 12:12:40 -0400

    Add test/logic/benchmarkPredicate.test.ts

---
+++ test/logic/benchmarkPredicate.test.ts
@@ -0,0 +1,7 @@
+import { benchmarkPredicate } from '../../src/logic/benchmarkPredicate';
+
+describe('benchmarkPredicate', () => {
+  it('should be a function', () => {
+    expect(typeof benchmarkPredicate).toBe('function');
+  });
+});
