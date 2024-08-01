a2f7ddf15c5a4539e00189a64b65984cb2a5e47c
dailker <ilkeroztbm@gmail.com>
2024-07-31 20:44:44 -0400

    Add test/logic/assertAllPredicates.test.ts

---
+++ test/logic/assertAllPredicates.test.ts
@@ -0,0 +1,7 @@
+import { assertAllPredicates } from '../../src/logic/assertAllPredicates';
+
+describe('assertAllPredicates', () => {
+  it('should be a function', () => {
+    expect(typeof assertAllPredicates).toBe('function');
+  });
+});
