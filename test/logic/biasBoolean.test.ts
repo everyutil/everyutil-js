42febed5e52a6b502f458d1ed27d5e841202b7e6
dailker <ilkeroztbm@gmail.com>
2024-08-01 20:53:03 -0400

    Add test/logic/biasBoolean.test.ts

---
+++ test/logic/biasBoolean.test.ts
@@ -0,0 +1,7 @@
+import { biasBoolean } from '../../src/logic/biasBoolean';
+
+describe('biasBoolean', () => {
+  it('should be a function', () => {
+    expect(typeof biasBoolean).toBe('function');
+  });
+});
