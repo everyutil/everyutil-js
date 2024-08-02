09dbb7e6920caeec2a3c7bbc13fdf9d0d2db015b
dailker <ilkeroztbm@gmail.com>
2024-08-02 14:29:48 -0400

    Add test/logic/boolClusterCount.test.ts

---
+++ test/logic/boolClusterCount.test.ts
@@ -0,0 +1,7 @@
+import { boolClusterCount } from '../../src/logic/boolClusterCount';
+
+describe('boolClusterCount', () => {
+  it('should be a function', () => {
+    expect(typeof boolClusterCount).toBe('function');
+  });
+});
