0cfedb4d153f1d9b87b09a708144ba360c4066f4
dailker <ilkeroztbm@gmail.com>
2024-08-02 14:16:31 -0400

    Add test/logic/boolByProbability.test.ts

---
+++ test/logic/boolByProbability.test.ts
@@ -0,0 +1,7 @@
+import { boolByProbability } from '../../src/logic/boolByProbability';
+
+describe('boolByProbability', () => {
+  it('should be a function', () => {
+    expect(typeof boolByProbability).toBe('function');
+  });
+});
