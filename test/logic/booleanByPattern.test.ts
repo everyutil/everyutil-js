6d76bd94e84e0ed11281cd1a1bfd4a261f7ec465
dailker <ilkeroztbm@gmail.com>
2024-08-03 08:10:26 -0400

    Add test/logic/booleanByPattern.test.ts

---
+++ test/logic/booleanByPattern.test.ts
@@ -0,0 +1,7 @@
+import { booleanByPattern } from '../../src/logic/booleanByPattern';
+
+describe('booleanByPattern', () => {
+  it('should be a function', () => {
+    expect(typeof booleanByPattern).toBe('function');
+  });
+});
