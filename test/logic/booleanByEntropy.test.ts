c3aaa6d6c5e6367372e2b35f9dc701be52b886ca
dailker <ilkeroztbm@gmail.com>
2024-08-03 09:00:22 -0400

    Add test/logic/booleanByEntropy.test.ts

---
+++ test/logic/booleanByEntropy.test.ts
@@ -0,0 +1,7 @@
+import { booleanByEntropy } from '../../src/logic/booleanByEntropy';
+
+describe('booleanByEntropy', () => {
+  it('should be a function', () => {
+    expect(typeof booleanByEntropy).toBe('function');
+  });
+});
